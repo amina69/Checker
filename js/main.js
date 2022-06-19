9dbf8c91e7b0c789028f44af4601c01771501af7
amina69 <sheriffamina1@gmail.com>
2022-06-19 03:41:15 +0100

    interface changes

--- js/main.js
+++ js/main.js
@@ -1,18 +1,14 @@
 let logout = document.querySelector(".logout");
-let input = document.querySelectorAll(".input");
 
 
 function login() {
     window.setTimeout(() => {
         window.open("login.html","_self")
-    }, 4000)
+    }, 2000)
 }
 
 logout.addEventListener("click", () => {
-    window.open("index.html","_self")
+    window.open("logout.html","_self");
+    window.close()
 })
 
-input.addEventListener("click", () => {
-    if (input.value > 0) {
-    this.classList.remove("icon")
-}})
