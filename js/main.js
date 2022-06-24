623a3b4ed4b8718f7b242cc570333c20f87c061f
amina69 <sheriffamina1@gmail.com>
2022-06-24 16:26:30 +0100

    responsiveness

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
-let logout = document.querySelector(".logout");
-
-
 function login() {
     window.setTimeout(() => {
         window.open("login.html","_self")
     }, 2000)
 }
 
-logout.addEventListener("click", () => {
-    window.open("logout.html","_self");
-    window.close()
-})
+window.onload = function() {
+    if (window.screen.width > 768) {
+        document.querySelector('.js-navbar').style.display = 'block';
+    }
+};
+
+let infoButton = document.querySelector(".button-2")
+
+infoButton.addEventListener("click", () => {
+    if (window.screen.width > 768) {
+        document.querySelector('.js-navbar').style.display = 'block';
+    }
+}
+
+)
+
+  
 
