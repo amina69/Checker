ed5a447f14406e775368f155c75d13071c338d50
amina69 <sheriffamina1@gmail.com>
2022-06-24 17:08:11 +0100

    responsiveness

--- js/main.js
+++ js/main.js
@@ -1,24 +1,13 @@
-function login() {
-    window.setTimeout(() => {
-        window.open("login.html","_self")
-    }, 2000)
-}
-
-window.onload = function() {
+function block() {
     if (window.screen.width > 768) {
         document.querySelector('.js-navbar').style.display = 'block';
     }
 };
 
-let infoButton = document.querySelector(".button-2")
-
-infoButton.addEventListener("click", () => {
-    if (window.screen.width > 768) {
-        document.querySelector('.js-navbar').style.display = 'block';
-    }
+function login() {
+    window.setTimeout(() => {
+        window.open("login.html","_self")
+    }, 2000)
 }
-
-)
-
   
 
