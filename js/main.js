d87f3b85a655cade038a8524b3f81a5fa765a3c7
amina69 <sheriffamina1@gmail.com>
2022-06-30 19:04:52 +0100

    responsiveness

--- js/main.js
+++ js/main.js
@@ -28,3 +28,26 @@ const toggleCallapse = (elemId, action = false) => {
             break;
     }
 }
+
+let inputClick = document.querySelectorAll('.button-click')
+
+inputClick.forEach(function(btn) {
+btn.addEventListener('click', function() {
+    
+    this.classList.add('inputClickAdd')
+
+    setTimeout(() => {
+        this.classList.remove("inputClickAdd")
+    }, 400);
+})
+});
+
+
+/*inputClick.addEventListener('click', () => {
+    let selectInput = document.querySelector(".row .input");
+    selectInput.classList.add('inputClickAdd')
+
+    setTimeout(() => {
+        selectInput.classList.remove("inputClickAdd")
+    }, 400);
+});*/
